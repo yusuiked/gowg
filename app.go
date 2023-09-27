f25d4334828ab269ccf4627cddbdeb479aff688b
yusuiked <yusuiked@gmail.com>
2023-09-27 14:20:20 +0900

    Added application menu and modified it to show about page

--- app.go
+++ app.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
+	wailsruntime "github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -24,4 +28,32 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) menu() *menu.Menu {
+	return &menu.Menu{
+		Items: []*menu.MenuItem{
+			{
+				Label: "GoWG", Role: menu.AppMenuRole,
+				SubMenu: &menu.Menu{
+					Items: []*menu.MenuItem{
+						{Label: "GoWG について", Click: func(cd *menu.CallbackData) { wailsruntime.EventsEmit(a.ctx, string(ShowAbout)) }},
+						{Label: "終了する", Accelerator: keys.CmdOrCtrl("q"), Click: func(cd *menu.CallbackData) { wailsruntime.Quit(a.ctx) }},
+					},
+				},
+			},
+			menu.EditMenu(),
+			menu.WindowMenu(),
+		},
+	}
+}
+
+type Channels string
+
+const (
+	ShowAbout = Channels("showAbout")
+)
+
+func (a *App) ChannelsShowAbout() Channels {
+	return ShowAbout
 }
